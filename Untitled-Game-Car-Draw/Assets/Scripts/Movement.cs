6435dd031a60941408ac22e0adcc73afff2ef383
RichardPilien <richardylan.pilien@gmail.com>
2024-06-07 02:27:35 +0800

    Moves The Player Automatically

---
+++ Untitled-Game-Car-Draw/Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Automatically move the player to the left
+        rb.velocity = new Vector3(-speed, rb.velocity.y, 0);
+
+        // Allow the player to jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
